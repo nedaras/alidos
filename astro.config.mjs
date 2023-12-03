d08568319c0a24500d9be90cd6b8b6c32c5c8b6d
nedaras <nedas.pranskunas@gmail.com>
2023-12-03 19:45:10 +0200

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import solidJs from "@astrojs/solid-js";
+
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
+export default defineConfig({
+  output: 'hybrid',
+  integrations: [tailwind(), solidJs()],
+  adapter: vercel()
+});
